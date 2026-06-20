22eb9c537cfcb2f5192e32a93b50ebaa36a34e01
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-06-21 02:22:06 +0400

    database configuration

---
+++ src/main/java/az/bazaar_ai/core_ms/model/entity/User.java
@@ -0,0 +1,81 @@
+package az.bazaar_ai.core_ms.model.entity;
+
+import az.bazaar_ai.core_ms.util.enums.UserRole;
+import az.bazaar_ai.core_ms.util.enums.UserStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @UuidGenerator
+    @GeneratedValue
+    UUID id;
+
+    @Column(name = "first_name", nullable = false)
+    String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    String email;
+
+    @Column(name = "phone", nullable = false, unique = true)
+    String phone;
+
+    @Column(name = "password", nullable = false)
+    String password;
+
+    @JdbcTypeCode(SqlTypes.VARBINARY)
+    @Column(name = "profile_photo")
+    byte[] profilePhoto;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_status", nullable = false)
+    UserStatus userStatus = UserStatus.ACTIVE;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_role", nullable = false)
+    UserRole userRole = UserRole.MERCHANT;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    Instant updatedAt;
+}
---
+++ src/main/java/az/bazaar_ai/core_ms/util/enums/UserStatus.java
@@ -0,0 +1,6 @@
+package az.bazaar_ai.core_ms.util.enums;
+
+public enum UserStatus {
+    ACTIVE,
+    INACTIVE
+}
